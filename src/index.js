e271793ce1966a99d330f8568405c8b13a5eda46
talksik <patel.arjun50@gmail.com>
2020-09-04 11:30:06 -0700

    setting up firebase deployment and hosting

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <h1>this site is atleast working</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
