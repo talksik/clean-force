a00579851fa228d4fa7275d13e6d136db5738d44
talksik <patel.arjun50@gmail.com>
2020-09-05 18:52:30 -0700

    responsiveness

--- src/components/rameelo/landing/landing.js
+++ src/components/rameelo/landing/landing.js
@@ -13,26 +13,16 @@ export default class Landing extends React.Component {
       speed: 300,
       slidesToShow: 4,
       slidesToScroll: 1,
-      centerMode: true,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true,
+            slidesToScroll: 1,
           },
         },
         {
           breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          },
-        },
-        {
-          breakpoint: 480,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
